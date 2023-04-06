0168d8bad9f4d6c6d5dd2f169a3b5d682a3fe4ae
ndeyuliis <nico.deyuliis@gmail.com>
2023-04-06 15:58:37 -0300

    add dot env

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,13 @@
+import {config} from 'dotenv'
+
+config()
+
 export default{
-    token: process.env.JWT_SECRET || 'secrectToken',
+
     DB:{
         URI: process.env.MONGODB_URI || 'mongodb://localhost/calculator',
         USER: process.env.MONGODB_USER,
         PASSWORD: process.env.MONGODB_PASS,
-    }
+    },
+
 }
