3c2f9a5feab81103a6e9114987ad748481cf86f3
ndeyuliis <nico.deyuliis@gmail.com>
2023-04-06 16:31:13 -0300

    fix problem with mongoose

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,10 +4,7 @@ config()
 
 export default{
 
-    DB:{
+    
         URI: process.env.MONGODB_URI || 'mongodb://localhost/calculator',
-        USER: process.env.MONGODB_USER,
-        PASSWORD: process.env.MONGODB_PASS,
-    },
-
+     
 }
