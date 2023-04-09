60968c4221b536b3edb87911fbb0b745af04bf76
ndeyuliis <nico.deyuliis@gmail.com>
2023-04-09 18:01:05 -0300

    fix match token with user

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,7 +1,9 @@
 import jwt from 'jsonwebtoken';
 import config from '../config/config'
+import User from '../models/user';
 
-const verifyToken = (req, res, next) => {
+
+const verifyToken = async (req, res, next) => {
 	const token = req.headers['token'];
 	if (!token) {
 		return res.status(403).send('A token is required for authentication');
@@ -9,6 +11,14 @@ const verifyToken = (req, res, next) => {
 	try {
 		const decoded = jwt.verify(token, config.jwtSecret);
 		req.user = decoded;
+		const existEmail = await User.findOne({ userName: req.body.userName});
+		console.log(existEmail?.token, token)
+		if(existEmail?.token === token){
+			
+		}else{
+			return res.status(401).send('Please login');
+		}
+
 	} catch (err) {
 		return res.status(401).send('Invalid Token');
 	}
